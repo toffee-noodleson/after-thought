e9220f8259df09405e6ab3aabefecb4c90f6a40f
lam_local <lam_local@EAV1093057.eac.ad.ea.com>
2025-02-11 22:19:38 -0800

    projectiles

--- autoloads/signal_manager.gd
+++ autoloads/signal_manager.gd
@@ -1,1 +1,3 @@
+extends Node
+
 signal player_input()
---
+++ projectile_resource/projectile.gd
@@ -0,0 +1,9 @@
+class_name ProjectileRes
+
+extends Resource
+
+@export var speed: float = 1.0
+@export var damage: float = 1.0
+@export var frequency: float = 1.0
+@export var lifetime: float = 5.0
+@export var movement_type := ""
