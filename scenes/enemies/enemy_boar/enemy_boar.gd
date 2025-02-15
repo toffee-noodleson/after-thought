7bb3df56404fdca0e36712e6d55e7ce34a5872e5
lam_local <lam_local@EAV1093057.eac.ad.ea.com>
2025-02-14 22:35:28 -0800

    start boar attack

--- scenes/enemies/enemy_boar/enemy_boar.gd
+++ scenes/enemies/enemy_boar/enemy_boar.gd
@@ -4,11 +4,12 @@ extends EnemyBase
 @onready var wall_detect: RayCast2D = $WallDetect
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-enum ENEMY_STATE {IDLE, ATTACK, HURT, DEATH}
+enum ENEMY_STATE {IDLE, WALK, ATTACK, HURT, DEATH}
 
 var _current_state: ENEMY_STATE
 var _player_detected: bool = false
 var _move_speed: float = -25
+var _attack_speed: float = -50
 var _flipped: bool = false
 
 # Called when the node enters the scene tree for the first time.
@@ -18,16 +19,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	call_deferred("detect_player")
-	call_deferred("detect_wall")
+	calculate_state()
 	
-	
-	calculate_state(_current_state)
-	
-	# no conditions yet so using idle
-	_current_state = ENEMY_STATE.IDLE
 
 func _physics_process(delta: float) -> void:
-	
+	detect_wall()
 	# apply gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -38,14 +34,30 @@ func _physics_process(delta: float) -> void:
 func move() -> void:
 	velocity.x = _move_speed
 
-func calculate_state(state: ENEMY_STATE) -> void:
+func attack() -> void:
+	velocity.x = _attack_speed
+
+func calculate_state() -> void:
+	
+	if velocity.x != 0 and _current_state != ENEMY_STATE.ATTACK:
+		set_state(ENEMY_STATE.WALK)
+	else:
+		set_state(ENEMY_STATE.IDLE)
+
+
+func set_state(state: ENEMY_STATE) -> void:
 	
 	if state == _current_state:
 		return
 	
+	_current_state = state
+	
 	match _current_state:
 		ENEMY_STATE.IDLE:
+			animated_sprite_2d.play("idle")
+		ENEMY_STATE.WALK:
 			animated_sprite_2d.play("walk")
+
 
 func detect_player() -> void:
 	if player_detect.is_colliding():
@@ -56,14 +68,13 @@ func detect_wall() -> void:
 		flip_actor()
 
 func flip_actor() -> void:
-	if _flipped:
-		animated_sprite_2d.flip_h = true
-		player_detect.rotation_degrees = -90
-		_move_speed = absf(_move_speed)
-		_flipped = false
-	else:
+	if animated_sprite_2d.flip_h:
 		animated_sprite_2d.flip_h = false
-		player_detect.rotation_degrees = 90
-		_move_speed *= -1
-		_flipped = true
-		
+
+	else:
+		animated_sprite_2d.flip_h = true
+
+	player_detect.rotate(PI)
+	wall_detect.rotate(PI)
+	_move_speed *= -1
+	_attack_speed *= -1
